96da85ce1b26e2155f0f05c40a86f07360ba332e
Liu <dual__luo@outlook.com>
2024-01-30 17:00:47 +0800

    main

--- src/main/verilog/BHT.v
+++ src/main/verilog/BHT.v
@@ -14,57 +14,57 @@ module BHT(
   reg [31:0] _RAND_1;
   reg [31:0] _RAND_2;
 `endif // RANDOMIZE_MEM_INIT
-  reg [27:0] bhtTable_tag [0:15]; // @[cmd5.sc 24:21]
-  wire [27:0] bhtTable_tag_bhtEntry_data; // @[cmd5.sc 24:21]
-  wire [3:0] bhtTable_tag_bhtEntry_addr; // @[cmd5.sc 24:21]
-  wire [27:0] bhtTable_tag_MPORT_data; // @[cmd5.sc 24:21]
-  wire [3:0] bhtTable_tag_MPORT_addr; // @[cmd5.sc 24:21]
-  wire  bhtTable_tag_MPORT_mask; // @[cmd5.sc 24:21]
-  wire  bhtTable_tag_MPORT_en; // @[cmd5.sc 24:21]
-  reg  bhtTable_valid [0:15]; // @[cmd5.sc 24:21]
-  wire  bhtTable_valid_bhtEntry_data; // @[cmd5.sc 24:21]
-  wire [3:0] bhtTable_valid_bhtEntry_addr; // @[cmd5.sc 24:21]
-  wire  bhtTable_valid_MPORT_data; // @[cmd5.sc 24:21]
-  wire [3:0] bhtTable_valid_MPORT_addr; // @[cmd5.sc 24:21]
-  wire  bhtTable_valid_MPORT_mask; // @[cmd5.sc 24:21]
-  wire  bhtTable_valid_MPORT_en; // @[cmd5.sc 24:21]
-  reg [31:0] bhtTable_target_pc [0:15]; // @[cmd5.sc 24:21]
-  wire [31:0] bhtTable_target_pc_bhtEntry_data; // @[cmd5.sc 24:21]
-  wire [3:0] bhtTable_target_pc_bhtEntry_addr; // @[cmd5.sc 24:21]
-  wire [31:0] bhtTable_target_pc_MPORT_data; // @[cmd5.sc 24:21]
-  wire [3:0] bhtTable_target_pc_MPORT_addr; // @[cmd5.sc 24:21]
-  wire  bhtTable_target_pc_MPORT_mask; // @[cmd5.sc 24:21]
-  wire  bhtTable_target_pc_MPORT_en; // @[cmd5.sc 24:21]
+  reg [27:0] bhtTable_tag [0:15]; // @[cmd2.sc 25:21]
+  wire [27:0] bhtTable_tag_bhtEntry_data; // @[cmd2.sc 25:21]
+  wire [3:0] bhtTable_tag_bhtEntry_addr; // @[cmd2.sc 25:21]
+  wire [27:0] bhtTable_tag_MPORT_data; // @[cmd2.sc 25:21]
+  wire [3:0] bhtTable_tag_MPORT_addr; // @[cmd2.sc 25:21]
+  wire  bhtTable_tag_MPORT_mask; // @[cmd2.sc 25:21]
+  wire  bhtTable_tag_MPORT_en; // @[cmd2.sc 25:21]
+  reg  bhtTable_valid [0:15]; // @[cmd2.sc 25:21]
+  wire  bhtTable_valid_bhtEntry_data; // @[cmd2.sc 25:21]
+  wire [3:0] bhtTable_valid_bhtEntry_addr; // @[cmd2.sc 25:21]
+  wire  bhtTable_valid_MPORT_data; // @[cmd2.sc 25:21]
+  wire [3:0] bhtTable_valid_MPORT_addr; // @[cmd2.sc 25:21]
+  wire  bhtTable_valid_MPORT_mask; // @[cmd2.sc 25:21]
+  wire  bhtTable_valid_MPORT_en; // @[cmd2.sc 25:21]
+  reg [31:0] bhtTable_target_pc [0:15]; // @[cmd2.sc 25:21]
+  wire [31:0] bhtTable_target_pc_bhtEntry_data; // @[cmd2.sc 25:21]
+  wire [3:0] bhtTable_target_pc_bhtEntry_addr; // @[cmd2.sc 25:21]
+  wire [31:0] bhtTable_target_pc_MPORT_data; // @[cmd2.sc 25:21]
+  wire [3:0] bhtTable_target_pc_MPORT_addr; // @[cmd2.sc 25:21]
+  wire  bhtTable_target_pc_MPORT_mask; // @[cmd2.sc 25:21]
+  wire  bhtTable_target_pc_MPORT_en; // @[cmd2.sc 25:21]
   assign bhtTable_tag_bhtEntry_addr = io_pc[3:0];
-  assign bhtTable_tag_bhtEntry_data = bhtTable_tag[bhtTable_tag_bhtEntry_addr]; // @[cmd5.sc 24:21]
+  assign bhtTable_tag_bhtEntry_data = bhtTable_tag[bhtTable_tag_bhtEntry_addr]; // @[cmd2.sc 25:21]
   assign bhtTable_tag_MPORT_data = io_mem_pc[31:4];
   assign bhtTable_tag_MPORT_addr = io_pc[3:0];
   assign bhtTable_tag_MPORT_mask = 1'h1;
   assign bhtTable_tag_MPORT_en = io_pcsrc;
   assign bhtTable_valid_bhtEntry_addr = io_pc[3:0];
-  assign bhtTable_valid_bhtEntry_data = bhtTable_valid[bhtTable_valid_bhtEntry_addr]; // @[cmd5.sc 24:21]
+  assign bhtTable_valid_bhtEntry_data = bhtTable_valid[bhtTable_valid_bhtEntry_addr]; // @[cmd2.sc 25:21]
   assign bhtTable_valid_MPORT_data = 1'h1;
   assign bhtTable_valid_MPORT_addr = io_pc[3:0];
   assign bhtTable_valid_MPORT_mask = 1'h1;
   assign bhtTable_valid_MPORT_en = io_pcsrc;
   assign bhtTable_target_pc_bhtEntry_addr = io_pc[3:0];
-  assign bhtTable_target_pc_bhtEntry_data = bhtTable_target_pc[bhtTable_target_pc_bhtEntry_addr]; // @[cmd5.sc 24:21]
+  assign bhtTable_target_pc_bhtEntry_data = bhtTable_target_pc[bhtTable_target_pc_bhtEntry_addr]; // @[cmd2.sc 25:21]
   assign bhtTable_target_pc_MPORT_data = io_target_pc;
   assign bhtTable_target_pc_MPORT_addr = io_pc[3:0];
   assign bhtTable_target_pc_MPORT_mask = 1'h1;
   assign bhtTable_target_pc_MPORT_en = io_pcsrc;
-  assign io_match = bhtTable_tag_bhtEntry_data == io_pc[31:4]; // @[cmd5.sc 30:30]
-  assign io_valid = bhtTable_valid_bhtEntry_data; // @[cmd5.sc 31:12]
-  assign io_bht_pred_pc = bhtTable_target_pc_bhtEntry_data; // @[cmd5.sc 32:18]
+  assign io_match = bhtTable_tag_bhtEntry_data == io_pc[31:4]; // @[cmd2.sc 31:30]
+  assign io_valid = bhtTable_valid_bhtEntry_data; // @[cmd2.sc 32:12]
+  assign io_bht_pred_pc = bhtTable_target_pc_bhtEntry_data; // @[cmd2.sc 33:18]
   always @(posedge clock) begin
     if(bhtTable_tag_MPORT_en & bhtTable_tag_MPORT_mask) begin
-      bhtTable_tag[bhtTable_tag_MPORT_addr] <= bhtTable_tag_MPORT_data; // @[cmd5.sc 24:21]
+      bhtTable_tag[bhtTable_tag_MPORT_addr] <= bhtTable_tag_MPORT_data; // @[cmd2.sc 25:21]
     end
     if(bhtTable_valid_MPORT_en & bhtTable_valid_MPORT_mask) begin
-      bhtTable_valid[bhtTable_valid_MPORT_addr] <= bhtTable_valid_MPORT_data; // @[cmd5.sc 24:21]
+      bhtTable_valid[bhtTable_valid_MPORT_addr] <= bhtTable_valid_MPORT_data; // @[cmd2.sc 25:21]
     end
     if(bhtTable_target_pc_MPORT_en & bhtTable_target_pc_MPORT_mask) begin
-      bhtTable_target_pc[bhtTable_target_pc_MPORT_addr] <= bhtTable_target_pc_MPORT_data; // @[cmd5.sc 24:21]
+      bhtTable_target_pc[bhtTable_target_pc_MPORT_addr] <= bhtTable_target_pc_MPORT_data; // @[cmd2.sc 25:21]
     end
   end
 // Register and memory initialization
